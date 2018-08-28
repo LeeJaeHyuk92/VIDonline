e7d4a93ea38401196d651e9ff26b89d0d7db8410
LeeJaeHyuk92 <leejh920917@gmail.com>
2018-08-28 20:39:10 +0900

    training code DETVID + hanning_window

--- src/main.py
+++ src/main.py
@@ -27,6 +27,12 @@ kGeneratedExamplesPerImage = POLICY['kGeneratedExamplesPerImage']
 run_config = tf.ConfigProto()
 run_config.gpu_options.allow_growth = True
 
+# hanning window, target image
+hann_1d = np.expand_dims(np.hanning(227), axis=0)
+hann_2d = np.transpose(hann_1d) * hann_1d
+hann_2d_3c = np.expand_dims(hann_2d, axis=2)
+  
+
 
 def train_image(image_loader, images):
     """TODO: Docstring for train_image.
@@ -41,7 +47,7 @@ def train_image(image_loader, images):
     # tracker_trainer.train(image, image, bbox, bbox)
 
 
-def train_video(videos, adj=True):
+def train_video(videos, adj=False):
     """TODO: Docstring for train_video.
     """
     video_num = np.random.randint(0, len(videos))
@@ -72,7 +78,7 @@ def train_video(videos, adj=True):
     # tracker_trainer.train(image_prev, image_curr, bbox_prev, bbox_curr)
 
 
-def data_reader_old_version(train_vid_videos):
+def data_reader(train_vid_videos):
     objExampleGen = example_generator(float(POLICY['lamda_shift']), float(POLICY['lamda_scale']),
                                       float(POLICY['min_scale']), float(POLICY['max_scale']), logger)
 
@@ -81,53 +87,19 @@ def data_reader_old_version(train_vid_videos):
     bbox_gt_scaleds = []
 
     for idx in xrange(POLICY['BATCH_SIZE']):
-        center_bool = False
-        while not center_bool:
-            img_prev, img_curr, bbox_prev, bbox_curr = train_video(train_vid_videos)
-            objExampleGen.reset(bbox_prev, bbox_curr, img_prev, img_curr)
-            image, target, bbox_gt_scaled = objExampleGen.make_true_example()
-            center_bool = check_center(bbox_gt_scaled)
-            if center_bool:
-                images.append(image)
-                targets.append(target)
-                bbox_gt_scaleds.append(bbox_gt_scaled)
-                images, targets, bbox_gt_scaleds = objExampleGen.make_training_examples(kGeneratedExamplesPerImage,
-                                                                                        images, targets, bbox_gt_scaleds)
-
-    # debug
-    # show_images(images, targets, bbox_gt_scaleds)
-
-    for idx, (img, tag, box) in enumerate(zip(images, targets, bbox_gt_scaleds)):
-        images[idx] = cv2.resize(img, (HEIGHT, WIDTH), interpolation=cv2.INTER_CUBIC)
-        targets[idx] = cv2.resize(tag, (HEIGHT, WIDTH), interpolation=cv2.INTER_CUBIC)
-        bbox_gt_scaleds[idx] = np.array([box.x1, box.y1, box.x2, box.y2], dtype=np.float32)
-
-    images = np.reshape(np.array(images), (len(images), 227, 227, 3))
-    targets = np.reshape(np.array(targets), (len(targets), 227, 227, 3))
-    bbox_gt_scaled = np.reshape(np.array(bbox_gt_scaleds), (len(bbox_gt_scaleds), 4))
-
-    return [images, targets, bbox_gt_scaled]
-
-def data_reader(train_vid_videos, adj=True):
-    objExampleGen = example_generator(float(POLICY['lamda_shift']), float(POLICY['lamda_scale']),
-                                      float(POLICY['min_scale']), float(POLICY['max_scale']), logger)
-
-    images = []
-    targets = []
-    bbox_gt_scaleds = []
-
-    for idx in xrange(POLICY['BATCH_SIZE']):
-        img_prev, img_curr, bbox_prev, bbox_curr = train_video(train_vid_videos, adj=adj)
+        img_prev, img_curr, bbox_prev, bbox_curr = train_video(train_vid_videos, adj=False)
         objExampleGen.reset(bbox_prev, bbox_curr, img_prev, img_curr)
         images, targets, bbox_gt_scaleds = objExampleGen.make_training_examples(kGeneratedExamplesPerImage,
                                                                                     images, targets, bbox_gt_scaleds)
 
     # debug
     # show_images(images, targets, bbox_gt_scaleds)
+   
 
     for idx, (img, tag, box) in enumerate(zip(images, targets, bbox_gt_scaleds)):
         images[idx] = cv2.resize(img, (HEIGHT, WIDTH), interpolation=cv2.INTER_CUBIC)
-        targets[idx] = cv2.resize(tag, (HEIGHT, WIDTH), interpolation=cv2.INTER_CUBIC)
+        tag = cv2.resize(tag, (HEIGHT, WIDTH), interpolation=cv2.INTER_CUBIC)
+        targets[idx] = np.multiply(tag, hann_2d_3c)
         bbox_gt_scaleds[idx] = np.array([box.x1, box.y1, box.x2, box.y2], dtype=np.float32)
 
     images = np.reshape(np.array(images), (len(images), 227, 227, 3))
@@ -135,7 +107,6 @@ def data_reader(train_vid_videos, adj=True):
     bbox_gt_scaled = np.reshape(np.array(bbox_gt_scaleds), (len(bbox_gt_scaleds), 4))
 
     return [images, targets, bbox_gt_scaled]
-
 
 
 def data_reader_DET(objLoaderImgNet, train_imagenet_images):
@@ -162,7 +133,8 @@ def data_reader_DET(objLoaderImgNet, train_imagenet_images):
 
     for idx, (img, tag, box) in enumerate(zip(images, targets, bbox_gt_scaleds)):
         images[idx] = cv2.resize(img, (HEIGHT, WIDTH), interpolation=cv2.INTER_CUBIC)
-        targets[idx] = cv2.resize(tag, (HEIGHT, WIDTH), interpolation=cv2.INTER_CUBIC)
+        tag = cv2.resize(tag, (HEIGHT, WIDTH), interpolation=cv2.INTER_CUBIC)
+        targets[idx] = np.multiply(tag, hann_2d_3c)
         bbox_gt_scaleds[idx] = np.array([box.x1, box.y1, box.x2, box.y2], dtype=np.float32)
 
     images = np.reshape(np.array(images), (len(images), 227, 227, 3))
@@ -182,32 +154,33 @@ if __name__ == "__main__":
     objLoaderImgNet = loader_imagenet(imagenet_folder, imagenet_annotations_folder, logger)
     train_imagenet_images = objLoaderImgNet.loaderImageNetDet()
 
+    logger.info('total training image size is: IMAGENET: ' + str(len(train_imagenet_images)))
+
     ###### Load vid training images and annotations #####
     vid_folder = os.path.join(POLICY['vid2015'], 'images')
     vid_annotations_folder = os.path.join(POLICY['vid2015'], 'gt')
     objLoaderVID = loader_vid(vid_folder, vid_annotations_folder, logger)
     objLoaderVID.loaderVID()
     train_vid_videos = objLoaderVID.get_videos()
-
+    
     vid_images = 0
     det_images = 0
     for vid_idx in xrange(len(train_vid_videos)):
         video = train_vid_videos[vid_idx]
         annos = video.annotations
         vid_images += len(annos)
-
+    
     total_image_size = vid_images
     logger.info('total training VID images size is: ' + str(vid_images))
     ###### Load vid training images and annotations #####
 
-
-    logger.info('total training image size is: IMAGENET: ' + str(len(train_imagenet_images)))
+ 
 
     # debug
-    cur_batch = data_reader(train_vid_videos, adj=False)
+    # cur_batch = data_reader(train_vid_videos)
     # cur_batch = data_reader_DET(objLoaderImgNet, train_imagenet_images)
 
-    total_images = len(train_imagenet_images)
+    total_images = vid_images + len(train_imagenet_images)
 
 
     # network initialization
@@ -262,13 +235,13 @@ if __name__ == "__main__":
 
     assign_op = global_step.assign(start)
     sess.run(assign_op)
-    model_saver = tf.train.Saver(max_to_keep=30)
+    model_saver = tf.train.Saver(max_to_keep=150)
 
     # train
     try:
         for i in range(start, int((float(total_images)) / BATCH_SIZE * NUM_EPOCHS)):
             # save every 1h
-            if i % int((total_images) / BATCH_SIZE) == 0:
+            if i % int((total_images) / BATCH_SIZE / 10) == 0:
                 logger.info("start epoch[%d]" % (int(float(i) / (total_images) * BATCH_SIZE)))
                 if i > start:
                     save_ckpt = "checkpoint.ckpt"
@@ -278,8 +251,11 @@ if __name__ == "__main__":
             start_time = time.time()
             # dataloader test
 
-            # cur_batch = data_reader(train_vid_videos)
-            cur_batch = data_reader_DET(objLoaderImgNet, train_imagenet_images)
+            if i % 2 == 0:
+                cur_batch = data_reader(train_vid_videos)
+            else:
+                cur_batch = data_reader_DET(objLoaderImgNet, train_imagenet_images)
+
             logger.debug('data_reader: time elapsed: %.3f' % (time.time() - start_time))
 
             start_time = time.time()
